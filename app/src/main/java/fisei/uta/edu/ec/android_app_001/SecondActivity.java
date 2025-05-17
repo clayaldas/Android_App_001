3255af3641f6e752a4f64be3740f5c0b4cc3bd81
Clay Aldas <clayaldas@gmail.com>
2025-05-17 13:05:41 -0500

    Terminado clase del sabado 17/mayo/2025

--- app/src/main/java/fisei/uta/edu/ec/android_app_001/SecondActivity.java
+++ app/src/main/java/fisei/uta/edu/ec/android_app_001/SecondActivity.java
@@ -1,10 +1,17 @@
 package fisei.uta.edu.ec.android_app_001;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,8 +19,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private int code = 1;
+
     //  Variable para mostrar los datos recibidos del: MainActivity
     private TextView textViewMessage;
+
+    //***********************************************************************************
+    // Nueva forma
+    //***********************************************************************************
+    ActivityResultLauncher<Intent> activityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult o) {
+                    // Comprobar si la ventana hija (ThirdActivity) se cerro correctamente
+                    if (o.getResultCode() == SecondActivity.RESULT_OK) {
+                        Intent intent = o.getData();
+
+                        textViewMessage.setText("Utilizando la nueva forma: " + intent.getDataString());
+                    }
+                }
+            }
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,18 +62,51 @@ public class SecondActivity extends AppCompatActivity {
         textViewMessage.setText("Usuario:" + user + "     " + "Clave: " + password);
     }
     public void onClicBrowser(View view) {
-
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+        startActivity(intent);
     }
     public void onClicCall(View view) {
-
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+59374834398"));
+        startActivity(intent);
     }
+
     public void onClicGoggleMaps(View view) {
-
+        // Coordenadas: longitud y latitud (Ecuador)
     }
-    public void onClicShowThirdActivity(View view) {
 
+    public void onClicShowThirdActivity(View view) {
+        // Intent intent = new Intent(this, ThirdActivity.class);
+        // startActivity(intent);
+
+        Intent intent = new Intent(this, ThirdActivity.class);
+
+        // Para cuando se regresa datos
+        //startActivityForResult(intent,code);
+
+        //***********************************************************************************
+        // Nueva forma de capturar los datos regresados desde un activity
+        //***********************************************************************************
+        activityResult.launch(intent);
     }
     public void onClicClose(View view) {
         finish();
+    }
+
+    //***********************************************************************************
+    // Forma anterior de capturar los datos devueltos por un activity
+    //***********************************************************************************
+    // Este evento ejecuta al cerrar la ventana que lo llamo (ThirdActivity)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        /*
+        // Comprobar si la ventana que se invoco (ThirdActivity) se cerro correctamente
+        if ((requestCode == code) && (resultCode == RESULT_OK))
+        {
+            textViewMessage.setText("Selecciono el item: " + data.getDataString());
+        }
+
+         */
     }
 }
--- app/src/main/java/fisei/uta/edu/ec/android_app_001/ThirdActivity.java
+++ app/src/main/java/fisei/uta/edu/ec/android_app_001/ThirdActivity.java
@@ -1,6 +1,14 @@
 package fisei.uta.edu.ec.android_app_001;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +16,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ThirdActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ThirdActivity extends AppCompatActivity {
+    private ListView listViewData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,47 @@ public class ThirdActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        listViewData = findViewById(R.id.listViewData);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, loadDataListView());
+
+        // Asociar los datos (adapter) al ListView para que se visualice
+        listViewData.setAdapter(adapter);
+
+        // Utilizar el evento (clic) dentro de un item para seleccionar
+        // el item presionado.
+        listViewData.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // Recuperar el item seleccionado
+                String itemSelected = (String) listViewData.getAdapter().getItem(i);
+
+                /*
+                Toast.makeText(getApplicationContext(), "Item seleccionado: " + itemSelected,
+                        Toast.LENGTH_SHORT).show();
+                 */
+
+                // Regresar el item seleccionado
+                Intent intent = new Intent();
+                intent.setData(Uri.parse(itemSelected));
+
+                setResult(Activity.RESULT_OK, intent);
+
+                finish();
+            }
+        });
+    }
+
+    private List<String> loadDataListView() {
+        List<String> list = new ArrayList<String>();
+
+        for (int i=1; i <=20; i++) {
+            String number = "Número: " + i;
+            list.add(number);
+        }
+
+        return  list;
     }
 }
